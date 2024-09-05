fd27c9e7863e798ca4e8c01381fa8c65443833c5
user <1505532618@qq.com>
2024-09-06 00:34:46 +0800

    保留cui-club一期完结.zip文件

--- cui-club/cui-club-gen/src/main/java/GenCodeMain.java
+++
@@ -1,9 +0,0 @@
-import com.cui.core.core.CodeGenerationCode;
-
-public class GenCodeMain {
-
-    public static void main(String[] args) {
-        CodeGenerationCode.doGenCode(new BizPutContextHandler());
-    }
-
-}
